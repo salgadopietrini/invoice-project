14e7d039d26ed3ec1cfe3125dbd8956ae7dd681e
Maria Alves <mariavbalves@gmail.com>
2021-11-02 15:23:06 +0000

    nem invoice container text

--- src/components/Invoice/InvoiceContainer.js
+++ src/components/Invoice/InvoiceContainer.js
@@ -1,5 +1,8 @@
 import React from "react";
 
 export default function InvoiceContainer() {
-  return <div>InvoiceContainer</div>;
+  return <div>InvoiceContainer
+  <h2>Your Invoice</h2>
+  
+  </div>;
 }
