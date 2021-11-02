edcadb3eb3bc02cb24fda222e24922ee384ea280
ManuelSalgado <manuel.salgado@bootcamp-upgrade.com>
2021-11-02 20:03:37 +0000

    add login placeholder functionality

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,58 +1,74 @@
 import "./Login.css";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import Dashboard from "@material-ui/icons/Dashboard";
 import axios from "axios";
-
+import { Context } from "../../App";
 
 export default function Login() {
-  const [user, setUser]= useState("");
-  const [pass, setPass]= useState("");
-  const [autenticated, setAutenticated] = useState("");
+  const { aunt, setAunt } = useContext(Context);
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
 
   useEffect(() => {
     axios
       .get("https://mocki.io/v1/ae918de4-a89f-4b95-abd7-f674e511e59c")
-      .then((response) => setAutenticated((response.data.username===user && response.data.password===pass )?response.data.succes:"false"))
+      .then((response) =>
+        setAunt(
+          response.data.username === user && response.data.password === pass
+            ? true
+            : false
+        )
+      )
       .catch((err) => console.log(err));
-  });
+  }, [user, pass, setAunt]);
 
-const SaveUsername = (event) =>{
-setUser(event.target.value)
-}
+  const saveUsername = (event) => {
+    setUser(event.target.value);
+  };
 
-const SavePassword = (event) =>{
-  setPass(event.target.value)
-}
+  const savePassword = (event) => {
+    setPass(event.target.value);
+  };
 
+  const submitUser = () => {};
 
-
-const SubmitUser = () =>{
-      console.log(autenticated);
-
-      if(autenticated==="true")
-        alert("login sucessful");
-      else
-        alert("bad login");
-
-
-}
-
-  return(
-  <div><center><br></br>
-  <Link to={"/"}>
-        <Dashboard className="dashboard" />
-      </Link><br></br>
-  <h1>Invoice Project</h1>
-  <div>
-  <br></br><br></br>
-    <input className="input" placeholder="Username" onChange={SaveUsername}/><br></br><br></br>
-    <input type="password" className="input"  placeholder="Password" onChange={SavePassword}/><br></br>
-    <br></br><button className="button" onClick={SubmitUser}>Submit</button>
-  </div>
-    
-
-  
-  
- </center></div> );
+  return (
+    <div>
+      <center>
+        <br></br>
+        <Link to={"/"}>
+          <Dashboard className="dashboard" />
+        </Link>
+        <br></br>
+        <h1>Invoice Project</h1>
+        <div>
+          <br></br>
+          <br></br>
+          <input
+            className="input"
+            placeholder="Username"
+            onChange={saveUsername}
+            value={user}
+          />
+          <br></br>
+          <br></br>
+          <input
+            type="password"
+            className="input"
+            placeholder="Password"
+            onChange={savePassword}
+            value={pass}
+          />
+          <br></br>
+          <br></br>
+          <Link to={aunt ? "/" : "/login"}>
+            <button className="button" onClick={submitUser}>
+              Submit
+            </button>
+          </Link>
+        </div>
+      </center>
+    </div>
+  );
 }
--- src/components/UserData/UserData.js
+++ src/components/UserData/UserData.js
@@ -1,5 +1,10 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { Context } from "../../App";
 export default function UserData() {
+  const { aunt } = useContext(Context);
+  if (aunt) {
+  } else {
+    window.location.href = "./login";
+  }
   return <div>UserData</div>;
 }
