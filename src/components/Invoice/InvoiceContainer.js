cfa1df9f7e4fb7088a9233166ccb418bcb6dcf5d
Maria Alves <mariavbalves@gmail.com>
2021-11-02 16:42:07 +0000

    invoice container

--- src/components/Invoice/InvoiceContainer.js
+++ src/components/Invoice/InvoiceContainer.js
@@ -1,8 +1,103 @@
 import React from "react";
 
+import Navbar from "../Navbar/Navbar";
+import { Link } from "react-router-dom";
+import Dashboard from '@material-ui/icons/Dashboard';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './InvoiceContainer.css';
+import {
+    Button,
+    Card,
+    CardGroup,
+    CardImg,
+    Col,
+    Container,
+    Form,
+    FormControl,
+    Image,
+    Row
+} from "react-bootstrap";
+
+
+
 export default function InvoiceContainer() {
-  return <div>InvoiceContainer
-  <h2>Your Invoice</h2>
+  return <div>
+
+   <Link to={"/"}>
+      <Dashboard className="dashboard"/>
+   </Link>
+   <Link to={"/userData"}>
+      <AccountCircle className="account"/>
+  </Link>
+
+  <h2>Your Invoice</h2> 
+  
+                                <Row>
+                                 
+                                    <Col xs={10} md={10} style={{padding: '20px', margin:'100px', marginTop: '20px'}} >
+                                        <div style={{backgroundColor: "#090F86"}} >
+                                        <h2>Invoice #2</h2>
+
+                                            <Container style={{padding: "40px"}}>
+                                                <Row>
+
+                                                    <Col xs={12} md={12} >
+                                                        <Card>
+                                                            <CardImg
+                                                                src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1630572642i/2165.jpg"
+                                                                alt="" height="200px"/>
+
+                                                            <Card.Footer>
+                                                                <small className="text-muted">Last updated 3 mins
+                                                                    ago</small>
+                                                            </Card.Footer>
+                                                        </Card>
+
+                                                    </Col>
+                                                    <Col xs={12} md={12}>
+                                                        <Card>
+                                                            <CardImg
+                                                                src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1630572642i/2165.jpg"
+                                                                alt="" height="200px"/>
+
+                                                            <Card.Footer>
+                                                                <small className="text-muted">Last updated 3 mins
+                                                                    ago</small>
+                                                            </Card.Footer>
+                                                        </Card>
+                                                    </Col>
+                                                    <Col xs={12} md={12}>
+                                                        <Card>
+                                                            <CardImg
+                                                                src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1630572642i/2165.jpg"
+                                                                alt=""height="200px"/>
+
+                                                            <Card.Footer>
+                                                                <small className="text-muted">Last updated 3 mins
+                                                                    ago</small>
+                                                            </Card.Footer>
+                                                        </Card>
+                                                    </Col>
+                                                   
+
+
+
+                                                </Row>
+                                            </Container>
+
+                                        </div>
+
+                                    </Col>
+                                      
+                                </Row>
+                   
+           
   
   </div>;
 }
+
+
+
+
