620e149ec2ca4f83e6e56d4227a374345a90e868
Igor Ponte <igor-igor@live.com.pt>
2021-11-02 11:51:47 +0000

    Create login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -22,7 +22,9 @@ const SubmitUser = () =>{
   <div><center>
   <h1>Login</h1>
   <div>
+  <label>Username:</label>
     <input onChange={SaveUsername}/>
+    <label>Password:</label>
     <input onChange={SavePassword}/>
     <br></br><button onClick={SubmitUser}>Submit</button>
   </div>
