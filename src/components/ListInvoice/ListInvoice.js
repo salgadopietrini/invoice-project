21db2738bbb16121ca0f8befebcd16bc9dc747aa
Maria Alves <mariavbalves@gmail.com>
2021-11-03 15:13:58 +0000

    List invoice done

--- src/components/ListInvoice/ListInvoice.js
+++ src/components/ListInvoice/ListInvoice.js
@@ -1,5 +1,10 @@
 import React, { useContext } from "react";
 import { Context } from "../../App";
+import { Link } from "react-router-dom";
+import Dashboard from "@material-ui/icons/Dashboard";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight"
+import "./ListInvoice.css";
 
 export default function ListInvoice() {
   const { invoiceList, aunt } = useContext(Context);
@@ -9,8 +14,26 @@ export default function ListInvoice() {
   }
   return (
     <div>
+     <Link to={"/"}>
+        <Dashboard className="dashboard" />
+      </Link>
+      <Link to={"/userData"}>
+        <AccountCircle className="account" />
+      </Link>
+      
       {invoiceList.length > 0 &&
-        invoiceList.map((elem) => <div>{elem.id}</div>)}
+        invoiceList.map((elem) => <div> 
+        <center>
+  <div className="container2">
+<div className="rightContent" >
+<Link to={"/"}><KeyboardArrowRight className="icons2"  ></KeyboardArrowRight></Link>
+</div><br></br>
+<div className="contentDiv2" ><h4>{elem.id}</h4>
+
+</div></div>
+</center>
+        
+        </div>)}
     </div>
   );
 }
