3cb5449b6f9174ed1910c8637aca4ba0a456ae95
Maria Alves <mariavbalves@gmail.com>
2021-11-03 10:08:08 +0000

    changes in home

--- src/components/Home/HomeContainer.js
+++ src/components/Home/HomeContainer.js
@@ -7,6 +7,18 @@ import { Link } from "react-router-dom";
 import Dashboard from "@material-ui/icons/Dashboard";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import "./Home.css";
+import {
+  Button,
+  Card,
+  CardGroup,
+  CardImg,
+  Col,
+  Container,
+  Form,
+  FormControl,
+  Image,
+  Row,
+} from "react-bootstrap";
 
 export default function HomeContainer() {
   const { products, addProduct, aunt } = useContext(Context);
@@ -25,6 +37,16 @@ export default function HomeContainer() {
         <AccountCircle className="account" />
       </Link>
 
+      
+<Row>
+        <Col
+          xs={8}
+          md={8}
+          style={{ padding: "20px", margin: "100px", marginTop: "20px" }}
+        >
+         <h2>Products</h2>
+          <div style={{ backgroundColor: "#090F86" }}>
+          
       <div style={{ display: "flex" }}>
         <div style={{ border: "2px solid", width: "200px", height: "700px" }}>
           {products.length > 0
@@ -33,10 +55,21 @@ export default function HomeContainer() {
               ))
             : "loading"}{" "}
         </div>
-        <div style={{ border: "2px solid", width: "200px" }}>
-          <Cart />
-        </div>
+      
       </div>
+</div>
+       </Col>
+        <Col xs={2} md={2}>
+              <div style={{backgroundColor: "#090F86", padding:"10px", marginTop: "20px"}} >
+                      <h2>Cart</h2>
+              <div style={{ border: "2px solid", width: "200px" }}>
+                  <Cart />
+               </div>
+               </div>
+
+        </Col>
+
+      </Row>
     </div>
   );
 }
